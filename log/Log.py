6025959c8605212656476b14ed72e299225530a3
Vladyslav Krutin <vkrutin66@gmail.com>
2019-03-01 13:34:57 +0200

    Message

--- log/Log.py
+++ log/Log.py
@@ -1,12 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
 class Log:
     @staticmethod
     def log(val):
         if val == 'reset':
-            print('Reset')
-            #file = open('output/log.txt', 'w',  encoding='utf-8')
-            #file.close()
+            file = open('output/log.txt', 'w',  encoding='utf-8')
+            file.close()
         else:
-            print(val)
-            #file = open('output/log.txt', 'a', encoding='utf-8')
-            #file.write(val + '\n')
-            #file.close()
+            file = open('output/log.txt', 'a', encoding='utf-8')
+            file.write(val + '\n')
+            file.close()
