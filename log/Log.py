caada4d36ac65eb9437ccb8969a6e4ea59bd2143
Vladyslav Krutin <vkrutin66@gmail.com>
2019-03-01 12:05:04 +0200

    Message

--- log/Log.py
+++ log/Log.py
@@ -2,9 +2,9 @@ class Log:
     @staticmethod
     def log(val):
         if val == 'reset':
-            file = open('output/log.txt', 'w')
+            file = open('output/log.txt', 'w',  encoding='utf8')
             file.close()
         else:
-            file = open('output/log.txt', 'a')
+            file = open('output/log.txt', 'a', encoding='utf8')
             file.write(val + '\n')
             file.close()
